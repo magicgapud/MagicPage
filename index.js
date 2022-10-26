986e6ae4278bfcb8ecbb42fa7332f78cae19856e
Magic Gapud <magicgapud214@gmail.com>
2022-10-26 23:25:17 +0800

    change in skills and about me

---
+++ index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+	let item1 = $('#item1');
+	let win = $(window)
+
+	win.on("scroll", function() {
+		let top = win.scrollTop() / 3;
+
+	item1.css('transform', 'translate('-26rem')
+
+	});
+
+});
+
+
+
+
