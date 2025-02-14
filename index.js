07a5b8aaa24baf43ac8b510b40f9ceed42ba8496
Magic Gapud <68890791+magicgapud@users.noreply.github.com>
2025-02-14 17:31:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 $(document).ready(function(){
 
-	let item1 = $('#item1');
-	let win = $(window)
+	let words = ['Certified Public Accountant', 'former Financial Auditor','Full-stack Developer', 'System Analyst', 'frustrated Artist'];
 
-	win.on("scroll", function() {
-		let top = win.scrollTop() / 3;
+	let charIndex = 0;
+	let wordIndex = 0;
+	const speed = 100;
+	const eraseSpeed = 50;
+	const delayBetweenWords = 1000;
 
-	item1.css('transform', 'translate('-26rem')
+	function typeEffect(){
+		if(charIndex < words[wordIndex].length){
+			document.getElementById("profession").textContent += words[wordIndex].charAt(charIndex);
+			charIndex++
+			setTimeout(typeEffect, speed);
+		} else {
+			setTimeout(eraseEffect, delayBetweenWords);
+		};
+	};
+
+	function eraseEffect(){
+		if(charIndex > 0){
+			document.getElementById("profession").textContent = words[wordIndex].substring(0,charIndex-1);
+			charIndex--;
+			setTimeout(eraseEffect, eraseSpeed);
+		}else{
+			wordIndex = (wordIndex + 1) % words.length;
+			setTimeout(typeEffect, delayBetweenWords);
+		}
+	};
+
+	typeEffect();
 
 	});
-
-});
 
 
 
